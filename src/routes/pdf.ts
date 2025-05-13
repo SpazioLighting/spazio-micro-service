137e12b128ae1d093951b6cf4d4e1190cf2f9e3c
lindileyabo <lindile@spazio.co.za>
2025-05-13 12:30:08 +0200

    WIP

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -29,28 +29,51 @@ router.post('/generate-pdf',async (req:Request<{},{},PDFRequest>,res:Response)=>
     ]
    });
    const page = await browser.newPage();
-   await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)")
-   await page.evaluate((title:string)=>{
-    document.title = title;
-   },fileName);
+   await Promise.all([
+     page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36)"),
+     page.evaluate((title:string)=>{
+     document.title = title;
+    },fileName),
+     page.emulateMediaType('screen'),
+     page.addStyleTag({
+      content: `
+      hr.hr--small:nth-child(n+1),.back-to-collection,.social-sharing ,#my_centered_buttons_main,.product-single__quantity,.breadcrumb,footer,header ,a.glink,#rfq-btn-730{
+          display: none !important;
+        }
+      `
+    }),
+   page.setRequestInterception(true),
+    page.on('request',(req)=>{
+     const url = req.url();
+     const resourceType = req.resourceType();
+     if (
+       url.includes('google-analytics')||
+       url.includes('facebook')||
+       url.includes('hotjar')||
+       url.includes('doubleclick')||
+       url.includes('googletagmanager')||
+       url.includes('cdn.shopify.com/s/javascript')||
+       resourceType === 'media'||
+       resourceType === 'prefetch'
+     ) {
+       req.abort();
+     } else {
+       req.continue();
+     }
+    })
+ 
+    
+   ])
 
-   await page.emulateMediaType('screen');
 
    
    if(!url) return;
     await page.goto(url,{
-    waitUntil:'networkidle2',
+    waitUntil:'domcontentloaded',
     timeout:60000
    });
 
-   await page.addStyleTag({
-    content: `
-    hr.hr--small:nth-child(n+1),.back-to-collection,.social-sharing ,#my_centered_buttons_main,.product-single__quantity,.breadcrumb,footer,header ,a.glink,#rfq-btn-730{
-        display: none !important;
-      }
-    `
-  });
-   await page.pdf({
+  await page.pdf({
     path:filePath,
     printBackground:true,
      format:"LEDGER",
@@ -62,7 +85,7 @@ router.post('/generate-pdf',async (req:Request<{},{},PDFRequest>,res:Response)=>
         bottom:'100px'
      },
      pageRanges:"1-1"
-   });
+   })
    const stats = statSync(filePath);
    if (stats.size === 0) {
     console.error('Generated PDF is empty')
